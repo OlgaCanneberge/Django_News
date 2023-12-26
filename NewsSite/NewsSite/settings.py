37c41770b01f86667a350644f9934efa527a66f9
degty <ola1234@yandex.ru>
2023-12-26 12:39:33 +0500

    commit

--- NewsSite/NewsSite/settings.py
+++ NewsSite/NewsSite/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i7jzs9=)d3n#*dnm*+gg9oant=vl_z3-=-ra2no5+*_r**3+o6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+#
+# ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
 
-
-# DEBUG = False
+DEBUG = False
 #
 # ALLOWED_HOSTS = ['127.0.0.1']
 
